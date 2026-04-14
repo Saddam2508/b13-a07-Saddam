fdf070af6d0eeff7d60a4b659e4c27c64e9b7938
Saddam <eyarinsultana@gmail.com>
2026-04-14 21:50:48 +0600

    chart added

---
+++ src/app/stats/page.tsx
@@ -0,0 +1,12 @@
+import TimelineChart from "@/components/timeline/TimelineChart";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto flex justify-center">
+      <TimelineChart />
+    </div>
+  );
+};
+
+export default page;
