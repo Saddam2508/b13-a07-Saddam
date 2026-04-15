538e25671a57bdf88bf5d29575ad28502da92d2d
Saddam <eyarinsultana@gmail.com>
2026-04-15 08:13:16 +0600

    chart page updated

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -2,13 +2,20 @@ import TimelineChart from "@/components/timeline/TimelineChart";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Chart | b13-a07-saddam",
+  title: "Friendship Analytics | b13-a07-saddam",
 };
 
 const page = () => {
   return (
-    <div className="container mx-auto flex justify-center">
-      <TimelineChart />
+    <div className="container mx-auto ">
+      <h2 className="text-3xl font-bold text-center sm:text-start my-3">
+        Friendship Analytics
+      </h2>
+
+      <div className="text-center flex items-center justify-center h-full rounded-lg shadow-md p-4 bg-white m-8 ">
+        {" "}
+        <TimelineChart />
+      </div>
     </div>
   );
 };
